576ef7a01fae9e9acdb31f1602ba67e90d440006
Austin Smith <trunk9slamchest@gmail.com>
2019-09-18 12:45:39 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+	def name=(new_name)
+		@this_dogs_name = new_name
+	end
+
+	def name
+		@this_dogs_name
+	end
+
+end
+
+# lassie = Dog.new
+# lassie.name = "Lassie"
+ 
+# p lassie.name #=> "Lassie"
